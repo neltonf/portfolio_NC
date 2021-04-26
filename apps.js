fdb7b8684007fafb82e857e95b0dab59e168cecf
Nelton Fernandes <47947664+neltonf@users.noreply.github.com>
2021-04-26 13:10:36 +0530

    navbar-mobile

---
+++ apps.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "navigation";
+  }
+} 
+
+myFunction();
